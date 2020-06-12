b3a44c06e85633e9fc07f21512411b60861da6a9
SemSnel <s.snel1@student.avans.nl>
2020-06-12 22:21:46 +0200

    deadlines resource

---
+++ StudyMate/app/Http/Controllers/DeadlineController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Deadline;
+use Illuminate\Http\Request;
+
+class DeadlineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Deadline  $deadline
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Deadline $deadline)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Deadline  $deadline
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Deadline $deadline)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Deadline  $deadline
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Deadline $deadline)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Deadline  $deadline
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Deadline $deadline)
+    {
+        //
+    }
+}
---
+++ StudyMate/database/factories/DeadlineFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Deadline;
+use Faker\Generator as Faker;
+
+$factory->define(Deadline::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
