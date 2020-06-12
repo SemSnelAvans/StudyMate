792706efcb4ee0115946c1dc1774b2cca19223f4
SemSnel <s.snel1@student.avans.nl>
2020-06-12 22:26:56 +0200

    ModuleStudent resource added

---
+++ StudyMate/app/Http/Controllers/ModuleStudentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ModuleStudent;
+use Illuminate\Http\Request;
+
+class ModuleStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ModuleStudent  $moduleStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ModuleStudent $moduleStudent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ModuleStudent  $moduleStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ModuleStudent $moduleStudent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ModuleStudent  $moduleStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ModuleStudent $moduleStudent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ModuleStudent  $moduleStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModuleStudent $moduleStudent)
+    {
+        //
+    }
+}
---
+++ StudyMate/database/factories/ModuleStudentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ModuleStudent;
+use Faker\Generator as Faker;
+
+$factory->define(ModuleStudent::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
